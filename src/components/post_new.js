d247ccd69b905c43f282c757f7cd7c850dc7db45
Kevin Cai <kevincai1979@gmail.com>
2018-03-29 14:42:48 -0700

    add style for validation

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -4,11 +4,19 @@ import { Field, reduxForm } from 'redux-form';
 
 class PostNew extends Component {
   renderField(field) {
+    const { meta: { touched, error } } = field; // Use ES6 to make code cleaner
+
+    const className = `form-group ${
+      touched && error ? 'has-danger' : '' // field.meta.touched && field.meta.error ? 'has-danger' : ''
+    }`;
+
+    //
+
     return (
-      <div className="form-group">
+      <div className={className}>
         <label>{field.label}</label>
         <input className="form-control" type="text" {...field.input} />
-        {field.meta.touched ? field.meta.error : ''}
+        <div className="text-help">{touched ? error : ''}</div>
       </div>
     );
   }
