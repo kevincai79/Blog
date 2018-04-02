a30fd2de459dc98309589ee14baad509165d865c
Kevin Cai <kevincai1979@gmail.com>
2018-04-02 10:25:56 -0700

    add the function that the page will go back the post index page after add a new post

--- src/components/post_new.js
+++ src/components/post_new.js
@@ -22,8 +22,9 @@ class PostNew extends Component {
   }
 
   onSubmit(values) {
-    // console.log(this);
-    this.props.createPost(values);
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
 
   render() {
