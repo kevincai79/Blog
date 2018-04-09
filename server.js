e73944d032ee3f2d16d7f245a29f2b2b41ccbab4
Kevin Cai <kevincai1979@gmail.com>
2018-04-08 19:44:30 -0700

    add a route for hellosign

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+app.get('/hellosign', (req, res) => {
+  res.send(200);
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
 });
